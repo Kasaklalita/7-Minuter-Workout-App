a108ce54cb13e9bfb77f70a920c4fb0404210040
Veniamin Polienko <kasaklalita@mail.ru>
2022-10-01 15:55:20 +0300

    Using CounDownTimer to display the seconds remaining

--- app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.a7minuteworkout
 
 import android.content.Intent
+import android.content.IntentSender.OnFinished
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_exercise.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
