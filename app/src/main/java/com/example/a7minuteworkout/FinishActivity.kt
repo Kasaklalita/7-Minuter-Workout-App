92c367c931b0b1ddda56748ccb2a8ecb92f596ac
Veniamin Polienko <kasaklalita@mail.ru>
2022-10-09 08:34:53 +0300

    Adding the sqlite open helper and storing date in the db

--- app/src/main/java/com/example/a7minuteworkout/FinishActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/FinishActivity.kt
@@ -1,11 +1,17 @@
 package com.example.a7minuteworkout
 
+import android.icu.util.Calendar
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_finish.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
-
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
@@ -20,5 +26,21 @@ class FinishActivity : AppCompatActivity() {
         btnFinish.setOnClickListener {
             finish()
         }
+
+        addDateToDatabase()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun addDateToDatabase() {
+        val calendar = Calendar.getInstance()
+        val dateTime = calendar.time
+        Log.i("DATE: ", "$dateTime")
+
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format((dateTime))
+
+        val dbHandler = SqliteOpenHelper(this, null)
+        dbHandler.addDate(date)
+        Log.i("DATE: ", "Added")
     }
 }
